c490e9cc1bde18fd2dd7be0cf8502258eb315d67
Arnav Gupta <arnav@codingblocks.com>
2017-09-27 18:21:44 +0530

    use configs correctly in sql
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- src/handlers/SQLHandler.js
+++ src/handlers/SQLHandler.js
@@ -2,24 +2,14 @@ const SQLStore = require('jsonapi-store-relationaldb')
 const debug = require('debug')('SQL')
 const config = require('../../config')
 
-exports.Handler = new SQLStore({
-  dialect: config.DB_CONFIG.DB_DIALECT,
-  host: config.DB_CONFIG.DB_HOST,
-  port: config.DB_CONFIG.DB_PORT,
-  database: config.DB_CONFIG.DB_NAME, // If not provided, defaults to the name of the resource
-  username: config.DB_CONFIG.DB_USER,
-  password: config.DB_CONFIG.DB_PASS,
-  logging: debug
-})
-
 exports.createHandler = function () {
   return new SQLStore({
-    dialect: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    database: 'puzzlebounty', // If not provided, defaults to the name of the resource
-    username: 'puzzleuser',
-    password: 'puzzlepass',
+    dialect: config.DB_CONFIG.DB_DIALECT,
+    host: config.DB_CONFIG.DB_HOST,
+    port: config.DB_CONFIG.DB_PORT,
+    database: config.DB_CONFIG.DB_NAME, // If not provided, defaults to the name of the resource
+    username: config.DB_CONFIG.DB_USER,
+    password: config.DB_CONFIG.DB_PASS,
     logging: debug
   })
 }
